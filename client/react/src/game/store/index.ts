029cc85837fa39e88755f2f4764be4cc0fba30c3
theljwhite <theljwhite@gmail.com>
2024-10-05 12:53:28 -0400

    fix init state

--- client/react/src/game/store/index.ts
+++ client/react/src/game/store/index.ts
@@ -96,8 +96,7 @@ export const useGameStore = create<GameState>((set, get) => {
     userIncorrectGuesses: [],
     correctGuesses: [],
     difficulty: "Medium" as Difficulty,
-    // mistakes: STEP_DIFFICULTIES["Medium"][0].mistakes,
-    mistakes: 1,
+    mistakes: STEP_DIFFICULTIES["Medium"][0].mistakes,
     didWin: false,
     isSoundOn: false,
   };
